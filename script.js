c5e6f7286d440cb412b327795461a71bc0b8d45f
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-07-16 12:16:08 +0300

    del console.log

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const btns = document.querySelectorAll('.btn');
 
 const spoiler = (e) => {
-	console.log(e.target);
 	e.target.classList.toggle('btnToclose');
-	e.target.parentElement.firstElementChild.classList.toggle('spoiler-active');
+	e.target.previousElementSibling.classList.toggle('spoiler-active');
 	e.target.parentElement.nextElementSibling.classList.toggle('spoiler-text');
 };
 
