ba3e3dfe6259ecad24e77dcfe986d393ff449a4c
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-07-16 12:07:28 +0300

    fix bug with button and refactoring styles

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const btns = document.querySelectorAll('.btn');
 
 const spoiler = (e) => {
-	e.target.parentElement.classList.toggle('btnToclose');
-	e.target.parentElement.previousElementSibling.classList.toggle('spoiler-active');
-	e.target.parentElement.parentElement.nextElementSibling.classList.toggle('spoiler-text');
+	console.log(e.target);
+	e.target.classList.toggle('btnToclose');
+	e.target.parentElement.firstElementChild.classList.toggle('spoiler-active');
+	e.target.parentElement.nextElementSibling.classList.toggle('spoiler-text');
 };
 
 btns.forEach((btn) => {
